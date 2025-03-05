e9ab446c1dc465f4c4892e962f6ecd49367f087d
joseLaptop <josenarvaezcariaga@gmail.com>
2025-03-04 19:49:55 -0500

    update:pokemon model api mapper in pokemon entity  .var updated with official api url

--- src/modules/services/PokemonService.ts
+++ src/modules/services/PokemonService.ts
@@ -3,25 +3,31 @@ import { Config } from "../enviroment/Config";
 import { Pokemon } from "../types/Pokemon";
 
 export class PokemonService {
-    static async getAll(): Promise<Pokemon[]> {
+    static async getAll(limit?: number): Promise<Pokemon[]> {
         const url = Config.POKEMON_SERVICE_URL;
+
         const response = await axios.get(url);
-        return response.data.results;
+        let results = response.data.results;
+
+        // Limitar el número de resultados si se proporciona un límite
+        if (limit) {
+            results = results.slice(0, limit);
+        }
+
+        // Obtiene más detalles de cada Pokémon usando su URL
+        const pokemons = await Promise.all(
+            results.map(async (result: any) => {
+                const pokemonResponse = await axios.get(result.url);
+                return Pokemon.mapPokemonData(pokemonResponse.data);
+            })
+        );  
+
+        return pokemons;
     }
+
     static async getOne(id: number): Promise<Pokemon> {
         const url = `${Config.POKEMON_SERVICE_URL}/${id}`;
         const response = await axios.get(url);
-        return response.data;
+        return Pokemon.mapPokemonData(response.data);
     }
 }
-
-
-
-/** idealmente hay que devolver un array de pokemons como:
- * {
- *    "id": 1, 
- *    "name": "bulbasaur"
- *    "image": "https://xdofficial-artwork/1.png",
- *    "types": ["grass", "poison"]
- * }
- */
