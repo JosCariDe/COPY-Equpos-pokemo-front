52610435a82769d9e72937bbc1d93da52b42f855
joseLaptop <josenarvaezcariaga@gmail.com>
2025-03-18 22:25:52 -0500

    fix

--- src/modules/types/Coach.ts
+++ src/modules/types/Coach.ts
@@ -5,3 +5,11 @@ export interface Entrenador {
     email: string;
     birthDate: Date;
 }
+
+export interface Trainer {
+    id: number;
+    fisrtName: string;
+    lastName: string;
+    email: string;
+    birthDate: Date;
+}
